a94f8a6057254b7538c74b5bfff0cde94f30210f
AnFuns <76437286+AnFuns@users.noreply.github.com>
2022-12-23 14:02:03 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -27,6 +27,8 @@ TablePrefix = ${DB_TABLEPREFIX}
 Server = ${REDIS_SERVER}
 Password = ${REDIS_PASSWORD}
 DB = ${REDIS_DB}
+[Slave]
+Secret = Or6IZ5DoP0nEzw07ZrT4v4AK3eX8oAq0EBT6fCKZAH0Xv0bBSa0Bjwri321SWeBQ
 EOF
 
 ## 运行
